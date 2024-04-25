0e69a1e15ad5c553e0e5c475f29f612bcab16d0e
Omoshlawi <lawiomosh3@gmail.com>
2024-04-25 11:32:02 +0300

    Done with auth

--- components/layout/user-nav.tsx
+++ components/layout/user-nav.tsx
@@ -21,7 +21,9 @@ export function UserNav() {
           <Button variant="ghost" className="relative h-8 w-8 rounded-full">
             <Avatar className="h-8 w-8">
               <AvatarImage src={user.image ?? ""} alt={user.name ?? ""} />
-              <AvatarFallback>{user.name?.[0]}</AvatarFallback>
+              <AvatarFallback>
+                {(user.name ?? user.email)[0].toUpperCase()}
+              </AvatarFallback>
             </Avatar>
           </Button>
         </DropdownMenuTrigger>
