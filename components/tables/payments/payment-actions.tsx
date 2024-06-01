e4df3ec56b0a63cda56e78e8e0870f39e24a7dfc
Omoshlawi <lawiomosh3@gmail.com>
2024-06-01 15:09:35 +0300

    Designed receipt or complete payment

--- components/tables/payments/payment-actions.tsx
+++ components/tables/payments/payment-actions.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Payment } from "@prisma/client";
 import React, { FC } from "react";
 import {
@@ -11,24 +11,36 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { MoreHorizontal } from "lucide-react";
+import Link from "next/link";
 
-const PaymentsActions:FC<{payment:Payment}> = ({payment}) => {
-  return <DropdownMenu>
-  <DropdownMenuTrigger asChild>
-    <Button variant="ghost" className="h-8 w-8 p-0">
-      <span className="sr-only">Open menu</span>
-      <MoreHorizontal className="h-4 w-4" />
-    </Button>
-  </DropdownMenuTrigger>
-  <DropdownMenuContent align="end">
-    <DropdownMenuLabel>Actions</DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    
-    <DropdownMenuItem>
-      Delete Property
-    </DropdownMenuItem>
-  </DropdownMenuContent>
-</DropdownMenu>
+const PaymentsActions: FC<{ payment: Payment }> = ({ payment }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+
+        {payment.complete && (
+          <DropdownMenuItem>
+            <Link href={`/dashboard/payments/${payment.id}`}>Get receipt</Link>
+          </DropdownMenuItem>
+        )}
+        {!payment.complete && (
+          <DropdownMenuItem>
+            <Link href={`/dashboard/properties/${payment.propertyId}/pay`}>
+              Complete Payment
+            </Link>
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default PaymentsActions;
