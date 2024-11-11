257e3fe884cdd14404d34975fc7b5016073a8471
Karume-lab <danielkarume@outlook.com>
2024-11-11 15:29:18 +0300

    feat: :sparkles: add sitemap and robots file

--- app/robots.ts
+++ app/robots.ts
@@ -4,26 +4,9 @@ export default function robots(): MetadataRoute.Robots {
     return {
         rules: [
             {
-                userAgent: 'facebookexternalhit',
-                allow: '/',
-            },
-            {
-                userAgent: 'Googlebot',
-                allow: '/',
-            },
-            {
-                userAgent: 'Twitterbot',
-                allow: '/',
-            },
-            {
-                userAgent: 'LinkedInBot',
-                allow: '/',
-            },
-            {
                 userAgent: '*',
                 allow: '/',
-                disallow: ['/api/', '/admin/'],
-            }
+            },
         ],
         sitemap: 'https://platven.ke/sitemap.xml',
     }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,19 +1,31 @@
-import { MetadataRoute } from 'next';
+import prisma from "@/prisma/client";
+import { MetadataRoute } from "next";
 
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const properties = await prisma.property.findMany({
+    where: {
+      listed: true,
+    },
+  });
 
-export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-        {
-            url: 'https://platven.ke/',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://platven.ke/about',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://platven.ke/contact',
-            lastModified: new Date(),
-        },
-    ];
+  const propertyUrls = properties.map((property) => ({
+    url: `https://platven.ke/properties/${property.id}`,
+    lastModified: new Date(property.updatedAt),
+  }));
+
+  return [
+    {
+      url: "https://platven.ke/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://platven.ke/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://platven.ke/contact",
+      lastModified: new Date(),
+    },
+    ...propertyUrls,
+  ];
 }
